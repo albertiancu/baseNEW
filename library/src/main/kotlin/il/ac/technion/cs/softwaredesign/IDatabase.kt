2a81000fc08b9fd95b09f1c14004399b166953cc
mikilandver <mlandver@gmail.com>
2020-04-27 16:17:08 +0300

    b

--- library/src/main/kotlin/il/ac/technion/cs/softwaredesign/IDatabase.kt
+++ library/src/main/kotlin/il/ac/technion/cs/softwaredesign/IDatabase.kt
@@ -1,7 +1,6 @@
 package il.ac.technion.cs.softwaredesign
 
 interface IDatabase {
-     fun contains(key: String): Boolean
      fun write(key: String, value: String)
      fun read(key: String) : String?
      fun delete(key: String)
